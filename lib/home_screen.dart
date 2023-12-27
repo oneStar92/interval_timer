35a2cdd2781810e6f599cafad88553fb7a6e96c7
Ayaan <onestar7984@gmail.com>
2023-12-27 20:25:36 +0900

    refactor: home_screen ListView UI 구현

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:interval_timer/interval_list_item_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -19,6 +21,45 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.transparent,
         scrolledUnderElevation: 0,
       ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.0.h, horizontal: 8.0.w),
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: ListView.builder(
+                  itemCount: dummyData.length,
+                  itemBuilder: (context, index) {
+                    // return IntervalListItem();
+                    return Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0.h, horizontal: 16.0.w),
+                      child: Container(
+                        height: (85.h * dummyData[index]) + (1.0 * (dummyData[index] - 1)),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10).w
+                        ),
+                        child: ListView.separated(
+                          itemCount: dummyData[index],
+                          itemBuilder: (context, index) {
+                            return IntervalListItem();
+                          },
+                          separatorBuilder: (context, index) {
+                            return Divider(height: 1.0,);
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
+final List<int> dummyData = [1, 3, 2];
