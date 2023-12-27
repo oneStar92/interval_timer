a31b144aacaf46eff4a2083de2a6c50623f48549
Ayaan <onestar7984@gmail.com>
2023-12-27 17:42:02 +0900

    feat: Interval Model Type 정의

---
+++ lib/Model/interval.dart
@@ -0,0 +1,17 @@
+class Interval {
+  final int readySecond;
+  final int cycleCount;
+  final int cycleBreakSecond;
+  final int roundCount;
+  final int exerciseSecond;
+  final int breakSecond;
+
+  const Interval({
+    required this.readySecond,
+    required this.cycleCount,
+    required this.cycleBreakSecond,
+    required this.roundCount,
+    required this.exerciseSecond,
+    required this.breakSecond,
+  });
+}
