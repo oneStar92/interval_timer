36c9310343f9f8479fc0b12847460c25338ceb1c
Ayaan <onestar7984@gmail.com>
2023-12-28 20:45:49 +0900

    feat: ListItemViewModel 정의

---
+++ lib/view_model/list_item_view_model.dart
@@ -0,0 +1,9 @@
+import 'package:interval_timer/Model/tabata_element.dart';
+
+final class ListItemViewModel {
+  final TabataElement element;
+
+  const ListItemViewModel({
+    required this.element,
+  });
+}
