5a02d3a346ddc40f798400bd02bee92540564798
Ayaan <onestar7984@gmail.com>
2023-12-28 12:26:54 +0900

    refactor: ExerciseStartButton Widget 사용

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:interval_timer/Widget/exercise_start_button.dart';
 import 'package:interval_timer/interval_list_item_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -50,56 +51,7 @@ class HomeScreen extends StatelessWidget {
                           },
                         ),
                       )
-                    : Container(
-                        height: 60.h,
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF0085FF),
-                          borderRadius: BorderRadius.circular(10).w,
-                        ),
-                        child: Padding(
-                          padding: REdgeInsets.symmetric(horizontal: 8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              SizedBox(
-                                height: 30.w,
-                                width: 30.w,
-                                child: const FittedBox(
-                                  child: Icon(
-                                    Icons.play_arrow,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: Padding(
-                                  padding: REdgeInsets.symmetric(horizontal: 8.0),
-                                  child: Text(
-                                    '운동 시작',
-                                    textAlign: TextAlign.left,
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 28.sp,
-                                      fontFamily: 'Suite',
-                                      fontWeight: FontWeight.w800,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                '12:40',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 24.sp,
-                                  fontFamily: 'Suite',
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                    : const ExerciseStartButton(),
               );
             },
           ),
