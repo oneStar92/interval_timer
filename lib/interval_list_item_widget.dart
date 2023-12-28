13c9de8fd6a35d35715652cb94e1c03d1e4cac78
Ayaan <onestar7984@gmail.com>
2023-12-28 12:18:26 +0900

    refactor: ListItem UI 구성 변경

--- lib/interval_list_item_widget.dart
+++ lib/interval_list_item_widget.dart
@@ -8,55 +8,59 @@ class IntervalListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 85.h,
-      width: ScreenUtil().screenWidth,
+      width: double.infinity,
       color: Colors.transparent,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            '준비',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: const Color(0xFFFFE812),
-              fontSize: 22.sp,
-              fontFamily: 'Suite',
-              fontWeight: FontWeight.w600,
+      child: Padding(
+        padding: REdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Text(
+                '준비',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: const Color(0xFFFFE812),
+                  fontSize: 26.sp,
+                  fontFamily: 'Suite',
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
             ),
-          ),
-          Row(
-            children: [
-              IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.remove,
-                  size: 30.h,
-                  color: Colors.black,
+            GestureDetector(
+              onTap: null,
+              child: SizedBox(
+                height: 20.w,
+                width: 20.w,
+                child: const FittedBox(
+                  child: Icon(Icons.remove),
                 ),
               ),
-              Expanded(
-                child: Text(
-                  '00:00',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: const Color(0xFFFFE812),
-                    fontSize: 32.sp,
-                    fontFamily: 'Suite',
-                    fontWeight: FontWeight.w800,
-                  ),
+            ),
+            Padding(
+              padding: REdgeInsets.symmetric(horizontal: 6),
+              child: Text(
+                '00:00',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 40.sp,
+                  fontFamily: 'Suite',
+                  fontWeight: FontWeight.w800,
                 ),
               ),
-              IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.add,
-                  size: 30.h,
-                  color: Colors.black,
+            ),
+            GestureDetector(
+              onTap: null,
+              child: SizedBox(
+                height: 20.w,
+                width: 20.w,
+                child: const FittedBox(
+                  child: Icon(Icons.add),
                 ),
               ),
-            ],
-          )
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
