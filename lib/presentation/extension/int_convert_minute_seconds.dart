a5f34c4c02281a4898ea4f12fcf853a2432866fa
Ayaan <onestar7984@gmail.com>
2023-12-29 14:36:36 +0900

    refactor: naming 변경 사항 적용

--- lib/presentation/extension/int_convert_minute_seconds.dart
+++ lib/presentation/extension/int_convert_minute_seconds.dart
@@ -1,4 +1,4 @@
-import 'package:tabata_timer/common/contants.dart';
+import 'package:tabata_timer/common/constants.dart';
 
 extension IntConvertMinuteSeconds on int {
   String convertMinuteSeconds() {
