9416c2d043f86fa66d50e5e874bd3e8be5207e7a
Ayaan <onestar7984@gmail.com>
2024-01-02 21:36:54 +0900

    feat: BaseViewHolder 추상화 타입 정의

---
+++ lib/presentation/abstract/base_view_holder.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+abstract class BaseViewHolder extends StatelessWidget {
+  final Function()? _onClick;
+
+  Widget createContent(BuildContext context);
+
+  const BaseViewHolder({
+    super.key,
+    Function()? onClick,
+  })  : _onClick = onClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _onClick?.call(),
+      child: createContent(context),
+    );
+  }
+}
