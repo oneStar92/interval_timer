8bca5c72bb600a52cc19a7df182a7c405a9f9cd6
Ayaan <onestar7984@gmail.com>
2024-01-03 14:23:05 +0900

    feat: tabataViewModelProvider 상수 추가

--- lib/presentation/di/view_model_provider.dart
+++ lib/presentation/di/view_model_provider.dart
@@ -2,8 +2,11 @@ import 'package:tabata_timer/common/constants.dart';
 import 'package:tabata_timer/presentation/home/home_view_model.dart';
 import 'package:provider/provider.dart';
 import 'package:tabata_timer/presentation/home/list_item_view_model.dart';
+import 'package:tabata_timer/presentation/model/tabata.dart';
+import 'package:tabata_timer/presentation/tabata/tabata_view_model.dart';
 
 final homeViewModelProvider = ChangeNotifierProvider(create: (_) => HomeViewModel());
+final tabataViewModelProvider = ChangeNotifierProvider(create: (_) => TabataViewModel(tabata: Tabata()));
 final listItemViewModels = homeScreenRowsGroupedBySection
     .map((section) => section.map((row) => ListItemViewModel(element: row)).toList())
     .toList();
