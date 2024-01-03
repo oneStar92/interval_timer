8e5aebcc02caa5201bd964b50e133a76259b023b
Ayaan <onestar7984@gmail.com>
2024-01-03 16:33:15 +0900

    feat: CountSettingView 구현

---
+++ lib/presentation/home/component/count_setting_view.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tabata_timer/common/custom_colors.dart';
+import 'package:tabata_timer/presentation/model/tabata_element.dart';
+import 'package:tabata_timer/presentation/view/count_view_holder.dart';
+import 'package:tabata_timer/presentation/view/icon_button_holder.dart';
+
+class CountSettingView<T extends ChangeNotifier> extends StatelessWidget {
+  final TabataElement _element;
+  final Function() _actionIncrease;
+  final Function() _actionDecrease;
+  final String Function(BuildContext, T) _countSelector;
+
+  const CountSettingView({
+    super.key,
+    required TabataElement element,
+    required Function() actionIncrease,
+    required Function() actionDecrease,
+    required String Function(BuildContext, T) countSelector,
+  })  : _element = element,
+        _actionIncrease = actionIncrease,
+        _actionDecrease = actionDecrease,
+        _countSelector = countSelector;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 85.h,
+      width: double.infinity,
+      padding: REdgeInsets.symmetric(horizontal: 16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10).w,
+        color: Colors.white,
+      ),
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Text(
+              _element.toString(),
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: CustomColors.getColorOf(_element),
+                fontSize: 18.sp,
+                fontFamily: 'Suite',
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20.w,
+            width: 20.w,
+            child: IconButtonHolder(
+              icon: Icons.remove,
+              onClick: _actionDecrease,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(
+            width: 100.w,
+            child: CountViewHolder(
+              textStyle: TextStyle(
+                fontSize: 40.sp,
+                fontFamily: 'Suite',
+                fontWeight: FontWeight.w800,
+              ),
+              selector: _countSelector,
+            ),
+          ),
+          SizedBox(
+            height: 20.w,
+            width: 20.w,
+            child: IconButtonHolder(
+              icon: Icons.add,
+              onClick: _actionIncrease,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
