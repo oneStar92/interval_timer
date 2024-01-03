62fc7f64f6f1ee0cd28ee30fffcf5daa287d040b
Ayaan <onestar7984@gmail.com>
2024-01-03 11:44:00 +0900

    feat: CustomColors Type 정의

---
+++ lib/common/custom_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:tabata_timer/presentation/model/tabata_element.dart';
+
+final class CustomColors {
+  static const Color blueColor = Color(0xFF0085FF);
+  static const Color yellowColor = Color(0xFFFFE812);
+  static const Color greenColor = Color(0xFF34A853);
+  static const Color redColor = Color(0xFFEA4335);
+  static const Color orangeColor = Color(0xFFFBBC04);
+
+  static Color getColorOf(TabataElement element) {
+    switch (element) {
+      case TabataElement.preparationTime:
+        return yellowColor;
+      case TabataElement.cycle:
+        return Colors.black;
+      case TabataElement.cycleBreakTime:
+        return orangeColor;
+      case TabataElement.round:
+        return Colors.black;
+      case TabataElement.exerciseTime:
+        return greenColor;
+      case TabataElement.breakTime:
+        return redColor;
+    }
+  }
+}
