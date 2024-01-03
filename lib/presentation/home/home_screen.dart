24f4eab456d6a9b6ac7eff799384d67847c4863e
Ayaan <onestar7984@gmail.com>
2024-01-03 16:36:23 +0900

    refactor: HomeScreen UI 재구성

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import 'package:tabata_timer/common/constants.dart';
-import 'package:tabata_timer/presentation/di/view_model_provider.dart';
-import 'package:tabata_timer/presentation/home/component/exercise_start_button.dart';
-import 'package:tabata_timer/presentation/home/component/tabata_elements_list_item_view.dart';
+import 'package:tabata_timer/common/custom_colors.dart';
+import 'package:tabata_timer/presentation/home/component/count_setting_view.dart';
+import 'package:tabata_timer/presentation/home/component/tabata_start_button_holder.dart';
+import 'package:tabata_timer/presentation/home/component/time_setting_view.dart';
+import 'package:tabata_timer/presentation/home/home_view_model.dart';
+import 'package:tabata_timer/presentation/model/tabata_element.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -26,41 +30,48 @@ class HomeScreen extends StatelessWidget {
         scrolledUnderElevation: 0,
       ),
       body: SafeArea(
-        child: Padding(
-          padding: REdgeInsets.all(8.0),
-          child: ListView.builder(
-            itemCount: sectionCount + 1,
-            itemBuilder: (context, section) {
-              return Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0.h, horizontal: 16.0.w),
-                child: section == 0
-                    ? const ExerciseStartButton()
-                    : LayoutBuilder(
-                        builder: (context, constraint) {
-                          final rowCount = homeScreenRowsGroupedBySection[section - 1].length;
-                          final height = (85.h * rowCount) + (1.0 * (rowCount - 1));
-                          return Container(
-                            height: height,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10).w,
-                            ),
-                            child: ListView.separated(
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemCount: rowCount,
-                              itemBuilder: (context, row) {
-                                return TabataElementsListItemView(
-                                  viewModel: listItemViewModels[section - 1][row],
-                                );
-                              },
-                              separatorBuilder: (_, index) {
-                                return const Divider(height: 1.0);
-                              },
-                            ),
-                          );
-                        },
-                      ),
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          padding: REdgeInsets.all(16.0),
+          child: ListView.separated(
+            itemCount: TabataElement.values.length + 1,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return Container(
+                  height: 60.h,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: CustomColors.blueColor,
+                    borderRadius: BorderRadius.circular(10).w,
+                  ),
+                  child: TabataStartButtonHolder<HomeViewModel>(
+                    timeSelector: (_, viewModel) => viewModel.totalTime,
+                  ),
+                );
+              } else {
+                final element = TabataElement.values[index - 1];
+                if (element == TabataElement.cycle || element == TabataElement.round) {
+                  return CountSettingView<HomeViewModel>(
+                    element: element,
+                    actionIncrease: () => context.read<HomeViewModel>().increase(element: element),
+                    actionDecrease: () => context.read<HomeViewModel>().decrease(element: element),
+                    countSelector: (_, viewModel) => viewModel.getCountOf(element),
+                  );
+                } else {
+                  return TimeSettingView<HomeViewModel>(
+                    element: element,
+                    actionIncrease: () => context.read<HomeViewModel>().increase(element: element),
+                    actionDecrease: () => context.read<HomeViewModel>().decrease(element: element),
+                    timeSelector: (_, viewModel) => viewModel.getTimeOf(element),
+                  );
+                }
+              }
+            },
+            separatorBuilder: (context, index) {
+              return Divider(
+                height: 8.0.h,
+                color: Colors.transparent,
               );
             },
           ),
