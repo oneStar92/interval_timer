6d8237f025ffe438836ced88365fbae6acb0e8d2
Ayaan <onestar7984@gmail.com>
2024-01-03 16:36:33 +0900

    refactor: 불필요한 파일 삭제

--- lib/presentation/home/list_item_view_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:tabata_timer/presentation/model/tabata_element.dart';
-
-final class ListItemViewModel {
-  final TabataElement element;
-
-  const ListItemViewModel({
-    required this.element,
-  });
-}
