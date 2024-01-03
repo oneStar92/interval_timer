4b0af93cb08b90098e8f03bc8f9bbb35a420d635
Ayaan <onestar7984@gmail.com>
2024-01-03 10:11:34 +0900

    refactor: BottomButtons UI 수정
    - Previous, Next 삭제
    - 크기 수정

--- lib/presentation/tabata/components/bottom_buttons_view.dart
+++ lib/presentation/tabata/components/bottom_buttons_view.dart
@@ -3,53 +3,30 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tabata_timer/presentation/tabata/components/icon_button_holder.dart';
 
 class BottomButtonsView extends StatelessWidget {
-  final Function()? actionPrevious;
   final Function()? actionPlay;
-  final Function()? actionNext;
 
   const BottomButtonsView({
     super.key,
-    this.actionPrevious,
     this.actionPlay,
-    this.actionNext,
   });
 
   @override
   Widget build(BuildContext context) {
-    final height = 50.0.h;
+    final height = 100.0.h;
     const width = double.infinity;
     return Container(
       height: height,
       width: width,
       color: Colors.transparent,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(
-            height: height,
-            width: height,
-            child: IconButtonHolder(
-              onClick: actionPrevious,
-              icon: Icons.keyboard_double_arrow_left,
-            ),
+      child: Center(
+        child: SizedBox(
+          height: 80.h,
+          width: 80.h,
+          child: IconButtonHolder(
+            onClick: actionPlay,
+            icon: Icons.play_arrow,
           ),
-          SizedBox(
-            height: height,
-            width: height,
-            child: IconButtonHolder(
-              onClick: actionPlay,
-              icon: Icons.play_arrow,
-            ),
-          ),
-          SizedBox(
-            height: height,
-            width: height,
-            child: IconButtonHolder(
-              onClick: actionNext,
-              icon: Icons.keyboard_double_arrow_right,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
