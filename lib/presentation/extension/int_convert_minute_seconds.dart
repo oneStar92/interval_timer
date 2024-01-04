3d52b04318397dd518e53c8d0e165ac80223b71e
Ayaan <onestar7984@gmail.com>
2024-01-04 09:40:28 +0900

    refactor: 안쓰는 파일 제거

--- lib/presentation/extension/int_convert_minute_seconds.dart
+++
@@ -1,9 +0,0 @@
-import 'package:tabata_timer/common/constants.dart';
-
-extension IntConvertMinuteSeconds on int {
-  String convertMinuteSeconds() {
-    final minute = '${this ~/ maximumMinute}'.padLeft(2, '0');
-    final seconds = '${this % maximumSeconds}'.padLeft(2, '0');
-    return '$minute:$seconds';
-  }
-}
