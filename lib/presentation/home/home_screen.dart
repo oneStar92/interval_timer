05d996a578b285c9651b94eebac3eeacd690528c
Ayaan <onestar7984@gmail.com>
2024-01-04 09:39:18 +0900

    refactor: _showCountPickerView, _showTimePickerView 기능 구현

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -3,8 +3,10 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:tabata_timer/common/constants.dart';
 import 'package:tabata_timer/common/custom_colors.dart';
+import 'package:tabata_timer/presentation/home/component/count_picker_view.dart';
 import 'package:tabata_timer/presentation/home/component/count_setting_view.dart';
 import 'package:tabata_timer/presentation/home/component/tabata_start_button_holder.dart';
+import 'package:tabata_timer/presentation/home/component/time_picker_view.dart';
 import 'package:tabata_timer/presentation/home/component/time_setting_view.dart';
 import 'package:tabata_timer/presentation/home/home_view_model.dart';
 import 'package:tabata_timer/presentation/model/tabata_element.dart';
@@ -56,6 +58,8 @@ class HomeScreen extends StatelessWidget {
                     element: element,
                     actionIncrease: () => context.read<HomeViewModel>().increase(element: element),
                     actionDecrease: () => context.read<HomeViewModel>().decrease(element: element),
+                    actionChangeCount: (count) => context.read<HomeViewModel>().updateCount(element, value: count),
+                    showCountPicker: () => _showCountPickerView(context, element),
                     countSelector: (_, viewModel) => viewModel.getCountOf(element),
                   );
                 } else {
@@ -64,6 +68,7 @@ class HomeScreen extends StatelessWidget {
                     actionIncrease: () => context.read<HomeViewModel>().increase(element: element),
                     actionDecrease: () => context.read<HomeViewModel>().decrease(element: element),
                     timeSelector: (_, viewModel) => viewModel.getTimeOf(element),
+                    showTimerPicker: () => _showTimePickerView(context, element),
                   );
                 }
               }
@@ -77,6 +82,44 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+
+  Future<void> _showCountPickerView(BuildContext context, TabataElement element) {
+    return showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SizedBox(
+          height: ScreenUtil().screenHeight * 0.33,
+          child: CountPickerView(
+            title: element.toString(),
+            initValue: context.read<HomeViewModel>().getIntegerCountOf(element),
+            actionComplete: (value) => context.read<HomeViewModel>().updateCount(element, value: value),
+            maximumCount: context.read<HomeViewModel>().getMaximumCountOf(element),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _showTimePickerView(BuildContext context, TabataElement element) {
+    return showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SizedBox(
+          height: ScreenUtil().screenHeight * 0.33,
+          child: TimePickerView(
+            title: element.toString(),
+            initMinute: context.read<HomeViewModel>().getIntegerMinuteOf(element),
+            initSecond: context.read<HomeViewModel>().getIntegerSecondOf(element),
+            actionComplete: (minute, second) => context.read<HomeViewModel>().updateTime(
+                  element,
+                  minute: minute,
+                  second: second,
+                ),
+          ),
+        );
+      },
     );
   }
 }
